e7a1dc13064bf22c4044fae012399a4014493290
HEESEOK <heeseok.chong@gmail.com>
2020-10-01 01:51:09 +0900

    + style in jsx

--- my-app/src/Potato.js
+++ my-app/src/Potato.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function Potato() {
   const name = 'potato';
-  return <h2>I love {name}</h2>;
+  const defaultSize = 16;
+  const style = {
+    backgroundColor: 'blue',
+    padding: '16px',
+    color: 'white',
+    fontSize: defaultSize + 10 + 'px',
+  };
+
+  return (
+    <div style={style}>
+      <h2>I love {name}</h2>
+    </div>
+  );
 }
 
 export default Potato;
