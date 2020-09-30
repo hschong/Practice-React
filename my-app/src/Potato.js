71e60b53e38186d649c235cef2bec822ca4a1dbd
HEESEOK <heeseok.chong@gmail.com>
2020-10-01 01:57:31 +0900

    + style using className

--- my-app/src/Potato.js
+++ my-app/src/Potato.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import './Potato.css';
 
 function Potato() {
   const name = 'potato';
-  const defaultSize = 16;
-  const style = {
-    backgroundColor: 'blue',
-    padding: '16px',
-    color: 'white',
-    fontSize: defaultSize + 10 + 'px',
-  };
+  //   const defaultSize = 16;
+  //   const style = {
+  //     backgroundColor: 'blue',
+  //     padding: '16px',
+  //     color: 'white',
+  //     fontSize: defaultSize + 10 + 'px',
+  //   };
 
   return (
-    <div style={style}>
+    // <div style={style}>
+    <div className='Potato'>
       <h2>I love {name}</h2>
     </div>
   );
