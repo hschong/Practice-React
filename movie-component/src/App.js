bc3fc49d63e4abc2fe72a7f9e08df42267922d15
HEESEOK <heeseok.chong@gmail.com>
2020-10-02 01:38:38 +0900

    + axios + async/await

--- movie-component/src/App.js
+++ movie-component/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import axios from 'axios';
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    console.log(movies);
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false, book: true });
-    }, 6000);
-    console.log('componentDidMount:', this.state);
+    this.getMovies();
   }
 
   render() {
