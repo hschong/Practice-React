49df7df34db156e57274bfa84217338299fc733f
HEESEOK <heeseok.chong@gmail.com>
2020-10-01 17:57:31 +0900

    + Dynamic Component Generation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import Potato from './Potato';
-import logo from './logo.svg';
+// import Potato from './Potato';
+// import logo from './logo.svg';
 import './App.css';
 
+const foodsILike = [
+  {
+    name: 'kimbab',
+    image: 'http://swisshansik.files.wordpress.com/2011/09/kimbap-2.jpg',
+  },
+  {
+    name: 'tteok-bokki',
+    image: 'https://t1.daumcdn.net/cfile/tistory/217BA24450D1717633',
+  },
+  {
+    name: 'bibimbap',
+    image: 'https://i.ytimg.com/vi/U_djLM9Z-qs/maxresdefault.jpg',
+  },
+];
+
 // function Food(props) {
-function Food({ fav }) {
-  // console.log(props.fav);
-  return <h1>I like {fav}</h1>;
+function Food({ foodName, foodPicture }) {
+  // console.log(props.name);
+  return (
+    <div>
+      <h1>I like {foodName}</h1>
+      <img src={foodPicture} width='400' alt={foodName} />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+        {/* <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -25,11 +45,10 @@ function App() {
         >
           Learn React
         </a>
-        <h1>React's components</h1>
-        <Potato />
-        <Food fav='kimchi' />
-        <Food fav='samgyupsal' />
-        <Food fav='Jjukumi' />
+        <Potato /> */}
+        {foodsILike.map((food) => (
+          <Food foodName={food.name} foodPicture={food.image} />
+        ))}
       </header>
     </div>
   );
