45928a855e46efa05b934d7e9c0d202ff1136d73
HEESEOK <heeseok.chong@gmail.com>
2020-10-01 19:06:53 +0900

    + PropTypes usages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,35 +2,46 @@ import React from 'react';
 // import Potato from './Potato';
 // import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
 
 const foodsILike = [
   {
     id: 1,
     name: 'kimbab',
     image: 'http://swisshansik.files.wordpress.com/2011/09/kimbap-2.jpg',
+    rating: 4.5,
   },
   {
     id: 2,
     name: 'tteok-bokki',
     image: 'https://t1.daumcdn.net/cfile/tistory/217BA24450D1717633',
+    rating: 4.2,
   },
   {
     id: 3,
     name: 'bibimbap',
     image: 'https://i.ytimg.com/vi/U_djLM9Z-qs/maxresdefault.jpg',
+    rating: 4.6,
   },
 ];
 
 // function Food(props) {
-function Food({ foodName, foodPicture }) {
+function Food({ foodName, foodPicture, foodRating }) {
   // console.log(props.name);
   return (
     <div>
-      <h1>I like {foodName}</h1>
+      <h2>I like {foodName}</h2>
+      <h4>{foodRating}/5</h4>
       <img src={foodPicture} width='400' alt={foodName} />
     </div>
   );
 }
+
+Food.propTypes = {
+  foodName: PropTypes.string.isRequired,
+  foodPicture: PropTypes.string.isRequired,
+  foodRating: PropTypes.number.isRequired,
+};
 
 function App() {
   return (
@@ -50,7 +61,12 @@ function App() {
         </a>
         <Potato /> */}
         {foodsILike.map((food) => (
-          <Food key={food.id} foodName={food.name} foodPicture={food.image} />
+          <Food
+            key={food.id}
+            foodName={food.name}
+            foodPicture={food.image}
+            foodRating={food.rating}
+          />
         ))}
       </header>
     </div>
