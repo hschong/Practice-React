0911af16ae4b4f4dfc23d3c4514620de54e3d80b
HEESEOK <heeseok.chong@gmail.com>
2020-10-02 20:27:49 +0900

    + Movie component + rendering Movie's title

--- movie-component/src/App.js
+++ movie-component/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+
 class App extends React.Component {
   state = {
     isLoading: true,
@@ -11,7 +13,9 @@ class App extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    } = await axios.get(
+      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
+    );
     this.setState({ movies, isLoading: false }); // ES6
     // this.setState({ movies: movies, isLoading: false }); // old style
   };
@@ -21,10 +25,24 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        <h1>{isLoading ? 'Loading...' : 'We are ready'}</h1>
+        {isLoading
+          ? 'Loading...'
+          : movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
       </div>
     );
   }
