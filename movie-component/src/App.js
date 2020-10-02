66ecc534aab51712a7f1897869ab5827f12f173e
HEESEOK <heeseok.chong@gmail.com>
2020-10-03 06:18:47 +0900

    + HashRouter

--- movie-component/src/App.js
+++ movie-component/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
-import About from './routes/About';
 import Home from './routes/Home';
+import About from './routes/About';
+import Navigation from './components/Navigation';
+import './App.css';
 
 function App() {
   return (
     <HashRouter>
+      <Navigation /> {/* <Navigation /> must be in HashRouter */}
       <Route path='/' exact={true} component={Home} />
-      <Route path='/about' exact={true} component={About} />
+      <Route path='/about' component={About} />
     </HashRouter>
+    // <footer>
+    // is need a fragment to render both at the same time.
+    // </footer>
   );
 }
 
